43ec64ad2b24b6c42607b4bc8436f67c8ddd1f9b
DulanjaliSankapaliKoswatte <s4084228@student.rmit.edu.au>
2025-09-08 11:52:34 +1000

    Test Vercel deploy

--- api/project/toc.Update.ts
+++ api/project/toc.Update.ts
@@ -1,8 +1,3 @@
-
-
-// ================================================================
-// UPDATE API Handler
-// ================================================================
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { ResponseUtils } from '../utils/ResponseUtils';
 import { CorsUtils } from '../utils/CorsUtils';
